8fa5f73e206d32d4e720ef4212d51f40e1578cac
CareerMonk <careermonk@users.noreply.github.com>
2017-03-25 22:39:32 +0530

    Update

--- src/chapter11searching/CheckDuplicatesSorting.py
+++ src/chapter11searching/CheckDuplicatesSorting.py
@@ -11,10 +11,9 @@
 def CheckDuplicatesSorting(A):
 	A.sort()
 	for i in range(0, len(A) - 1):
-		for j in range(i + 1, len(A)):
-			if(A[i] == A[i + 1]):
-				print("Duplicates exist:", A[i])
-				return;
+		if(A[i] == A[i + 1]):
+			print("Duplicates exist:", A[i])
+			return
 	print("No duplicates in given array.")
 
 A = [33, 2, 10, 20, 22, 32]
