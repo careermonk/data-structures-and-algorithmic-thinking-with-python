8cfc777e6277baadb0ec13d48b917843c45ca4c3
CareerMonk <careermonk@users.noreply.github.com>
2017-06-15 23:30:57 +0530

    Create finding_single_peak_with_linear_search.py

---
+++ src/chapter11searching/finding_single_peak_with_linear_search.py
@@ -0,0 +1,18 @@
+def find_peak(A):
+	peak = A[0]
+	for i in range(1, len(A)-2):
+		prev = A[i-1]
+		curr = A[i]
+		next = A[i+1]
+		if curr > prev and curr > next:
+			index = i
+			peak = curr
+			return peak
+
+        if len(A)-1 > peak:
+            return A[len(A)-1]
+ 
+        return A[index]
+
+A = [35, 5, 20, 2, 40, 25, 80, 25, 15, 40]
+print A, "\n", find_peak(A)
