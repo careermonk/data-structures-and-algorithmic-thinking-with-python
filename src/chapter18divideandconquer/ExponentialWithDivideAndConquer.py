49aba6b4c4f4341f7ba0c87379098993c3e9bd11
CareerMonk <careermonk@users.noreply.github.com>
2017-07-03 13:16:17 +0530

    Update ExponentialWithDivideAndConquer.py

--- src/chapter18divideandconquer/ExponentialWithDivideAndConquer.py
+++ src/chapter18divideandconquer/ExponentialWithDivideAndConquer.py
@@ -8,15 +8,34 @@
 # 				   warranty; without even the implied warranty of 
 # 				    merchantability or fitness for a particular purpose. 
 
-def power(k, n):
-    if n == 0: return 1
-    x = power(k, math.floor(n / 2))
-    if n % 2 == 0: return pow(x, 2)
-    else: return k * pow(x, 2)
+import math
+def power_brute_force(k, n):
+	"""linear power algorithm"""
+	x = k
+	for i in range(1, n):
+	    x *= k
+	return x
+
+print power_brute_force(2, 3)
+
 	    
-def powerBruteForce(k, n):
-    """linear power algorithm"""
-    x = k;
-    for i in range(1, n):
-        x *= k
-    return x	    
+
+def power_divide_and_conquer(k, n):
+"""Divide and Conquer power algorithm"""
+
+	# base case
+	if n == 0: 
+		return 1
+
+	# base case
+	if k == 0:
+		return 0
+
+	x = power_divide_and_conquer(a, math.floor(n/2))
+	if n % 2 == 0: 
+		return x * x
+	else: 
+		return k * x * x
+
+print power_divide_and_conquer(2, 4)
+    
