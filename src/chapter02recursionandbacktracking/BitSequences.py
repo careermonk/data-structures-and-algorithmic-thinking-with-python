93c115b8dcb18d2b3b2c005f0cc6e64e4e346e27
CareerMonk <careermonk@users.noreply.github.com>
2017-08-07 11:44:08 +0530

    Update BitSequences.py

--- src/chapter02recursionandbacktracking/BitSequences.py
+++ src/chapter02recursionandbacktracking/BitSequences.py
@@ -9,20 +9,20 @@
 # 				    merchantability or fitness for a particular purpose. 
 
 
-def append_at_beginning_front(x, L):
+def append_at_front(x, L):
     return [x + element for element in L]
 
 def bit_strings(n):
     if n == 0: return []
     if n == 1: return ["0", "1"]
     else:
-        return (append_at_beginning_front("0", bit_strings(n - 1)) + append_at_beginning_front("1", bit_strings(n - 1)))
+        return (append_at_front("0", bit_strings(n - 1)) + append_at_front("1", bit_strings(n - 1)))
                 
 print bit_strings(4)                      
 
 def bit_strings2(n):
     if n == 0: return []
     if n == 1: return ["0", "1"]
-    return [ digit + bitstring for digit in bit_strings2(1)  # @IndentOk
+    return [ digit + bitstring for digit in bit_strings2(1)
 	                         for bitstring in bit_strings2(n - 1)]
 print bit_strings2(4)                      
