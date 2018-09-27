7f2901548ec35df39160012cfe82a01074e26929
Abhimanyu Aryan <abhimanyu.chat@gmail.com>
2018-09-27 07:56:12 +0530

    Fixing bug in code

--- src/chapter19dynamicprogramming/LongestIncreasingSubSeq.py
+++ src/chapter19dynamicprogramming/LongestIncreasingSubSeq.py
@@ -1,21 +1,25 @@
 # Copyright (c) Dec 22, 2014 CareerMonk Publications and others.
 # E-Mail           		: info@careermonk.com 
-# Creation Date    		: 2014-01-10 06:15:46 
+# Creation Date    		: 2018-09-27 07:15:46 
 # Last modification		: 2008-10-31 
-#               by		: Narasimha Karumanchi 
+#               by		: Abhimanyu Aryan 
 # Book Title			: Data Structures And Algorithmic Thinking With Python
 # Warranty         		: This software is provided "as is" without any 
 # 				   warranty; without even the implied warranty of 
 # 				    merchantability or fitness for a particular purpose. 
 
-def LongestIncreasingSequence(numList):
-    LISTable = [1]
-    for i in range(1, len(numList))):
-        LISTable.append(1)
-        for j in range(i + 1, n):
-            if numList[i] < numList[j] and LISTable[i] < LISTable[j] + 1:
-                LISTable[i] = 1 + LISTable[j]
-    print LISTable
-    return max(LISTable)
+def LongestIncreasingSequence(arr): 
+    n = len(arr)  
+    LISTable = [1]*n  
+    for i in range (1 , n): 
+        for j in range(0 , i): 
+            if arr[i] > arr[j] and lis[i]< lis[j] + 1 : 
+                lis[i] = lis[j]+1
+    # Initialize maximum to 0 to get the maximum of all LIS
+    maximum = 0
+    # Pick maximum of all LIS values 
+    for i in range(n): 
+        maximum = max(maximum , lis[i]) 
+    return maximum 
 
-print LongestIncreasingSequence([3, 2, 6, 4, 5, 1])
+print(LongestIncreasingSequence([10, 22, 9, 33, 21, 50, 41, 60]))
