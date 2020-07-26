70488b6978743ea1f16ceadf1b234ee0012e9028
Sean Miranda <sean.miranda@gmail.com>
2020-07-27 00:15:29 +0530

    created folder for assignments

--- src/chapter05queues/QueueWithStacks.py
+++ src/chapter05queues/QueueWithStacks.py
@@ -1,4 +1,4 @@
-# Copyright (c) Dec 22, 2014 CareerMonk Publications and others.
+# Copyright (c) Oct 22, 2018 CareerMonk Publications and others.
 # E-Mail           		: info@careermonk.com 
 # Creation Date    		: 2014-01-10 06:15:46 
 # Last modification		: 2008-10-31 
@@ -8,24 +8,56 @@
 # 				   warranty; without even the implied warranty of 
 # 				    merchantability or fitness for a particular purpose. 
 
-class Queue(object):
+import random 
+
+class Stack:
+    def __init__(self, C = 5):
+        self.C = C
+        self.array = []
+    def size(self):
+        return len(self.array)
+    def isEmpty(self):
+        return len(self.array) == 0
+    def isFull(self):
+        return len(self.array) == self.C
+    def peek(self):
+        if self.isEmpty():
+            return None
+        return self.array[self.size()-1]
+    def pop(self):
+        if self.isEmpty():
+            print "Underflow"
+            return None
+        data = self.array.pop()
+        return data
+    def push(self, data):
+        if self.isFull():
+            print "Overflow"
+            return
+        self.array.append(data)
+
+class Queue:
     def __init__(self):
-        self.S1 = []
-        self.S2 = []
-     
+        self.s1 = Stack()
+        self.s2 = Stack()
     def enqueue(self, data):
-        self.S1.append(data)
-    
+        self.s1.push(data)
+
     def dequeue(self):
-        if not self.S2:
-            while self.S1:
-                self.S2.append(self.S1.pop())
-        return self.S2.pop() 
-	    
+		if (not self.s2.isEmpty()):
+			return self.s2.pop()
+		while(not self.s1.isEmpty()):
+			self.s2.push(self.s1.pop())
+		return self.s2.pop()
+
 q = Queue()
-for i in xrange(5):
-    q.enqueue(i)
-for i in xrange(5):
-    print q.dequeue()  
-    
-    
+q.enqueue(1)
+q.enqueue(6)
+q.enqueue(8)
+q.enqueue(10)
+q.enqueue(16)
+print q.dequeue()
+print q.dequeue()
+print q.dequeue()
+print q.dequeue()
+print q.dequeue()
